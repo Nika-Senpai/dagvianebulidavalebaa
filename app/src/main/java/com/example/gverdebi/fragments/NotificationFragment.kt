db7c1b3a8186cad7f40fcc01502213623571897e
Nika-Senpai <nikoloz.bakhtadze.1@btu.edu.ge>
2021-12-06 02:33:03 +0400

    Initial commit

---
+++ app/src/main/java/com/example/gverdebi/fragments/NotificationFragment.kt
@@ -0,0 +1,7 @@
+package com.example.gverdebi.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.gverdebi.R
+
+class NotificationFragment: Fragment(R.layout.fragment_notification) {
+}
---
+++ app/src/main/java/com/example/gverdebi/fragments/PhotoFragment.kt
@@ -0,0 +1,14 @@
+package com.example.gverdebi.fragments
+
+import android.os.Bundle
+import android.view.TextureView
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.gverdebi.R
+
+class PhotoFragment: Fragment(R.layout.fragment_photo) {
+
+
+
+}
